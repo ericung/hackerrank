231947584fa9de4d25e083c8401a3c43852721f1
Eric Ung <ungericw@tutanota.com>
2017-02-23 18:50:13 -0600

    day 11 2d arrays

---
+++ 10DayStatistics/Cpp/day_0_mean_median_mode.cc
@@ -0,0 +1,17 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+double mean(
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    cout.precision(1);
+    cout << fixed;
+
+    
+    return 0;
+}
---
+++ 30DayChallenge/Cpp/day_11_arrays.cc
@@ -0,0 +1,52 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main(){
+    vector< vector<int> > arr(6,vector<int>(6));
+    for(int arr_i = 0;arr_i < 6;arr_i++){
+        for(int arr_j = 0;arr_j < 6;arr_j++){
+            cin >> arr[arr_i][arr_j];
+        }
+    }
+
+    int max_sum = -64;
+    int sum;
+    for(int arr_i = 1; arr_i < 5; arr_i++){
+        sum = 0;
+        for(int arr_j = 1; arr_j < 5; arr_j++){
+            sum = arr[arr_i-1][arr_j-1] + arr[arr_i-1][arr_j] + 
+                arr[arr_i-1][arr_j+1] + arr[arr_i][arr_j] +
+                arr[arr_i+1][arr_j-1] + arr[arr_i+1][arr_j] + 
+                arr[arr_i+1][arr_j+1];
+            if (sum > max_sum) {
+                max_sum = sum;
+            }
+        }
+    }
+
+    cout << max_sum << endl;
+
+    return 0;
+}
+
